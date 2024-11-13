5d2bb7f743cd8acdd37f748eefc66f029465adbc
Alex123654 <alex.agra.og@gmail.com>
2024-11-13 08:47:02 -0400

    Proyecto de Grado

---
+++ proveedores/consultaProveedores.php
@@ -0,0 +1,159 @@
+<?php
+include ('../app/config.php');
+include ('../layout/sesion.php');
+
+include ('../layout/parte1.php');
+
+
+include ('../app/controllers/proveedores/listado_de_proveedores.php');
+
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Listado de Proveedores
+
+                    </h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-outline card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Proveedores registrados</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+
+                        </div>
+
+                        <div class="card-body" style="display: block;">
+                            <table id="example1" class="table table-bordered table-striped table-sm">
+                                <thead>
+                                <tr>
+                                    <th><center>Nro</center></th>
+                                    <th><center>Nombre del proveedor</center></th>
+                                    <th><center>Celular</center></th>
+                                    <th><center>Teléfono</center></th>
+                                    <th><center>Empresa</center></th>
+                                    <th><center>Email</center></th>
+                                    <th><center>Dirección</center></th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $contador = 0;
+                                foreach ($proveedores_datos as $proveedores_dato){
+                                    $id_proveedor = $proveedores_dato['id_proveedor'];
+                                    $nombre_proveedor = $proveedores_dato['nombre_proveedor']; ?>
+                                    <tr>
+                                        <td><center><?php echo $contador = $contador + 1;?></center></td>
+                                        <td><?php echo $nombre_proveedor;?></td>
+                                        <td>
+                                            <a href="https://wa.me/591<?php echo $proveedores_dato['celular'];?>" target="_blank" class="btn btn-success">
+                                                <i class="fa fa-phone"></i>
+                                                <?php echo $proveedores_dato['celular'];?>
+                                            </a>
+                                        </td>
+                                        <td><?php echo $proveedores_dato['telefono'];?></td>
+                                        <td><?php echo $proveedores_dato['empresa'];?></td>
+                                        <td><?php echo $proveedores_dato['email'];?></td>
+                                        <td><?php echo $proveedores_dato['direccion'];?></td>
+                                        <td>
+
+
+
+                                            
+
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+<?php include ('../layout/parte2.php'); ?>
+
+
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "pageLength": 5,
+            "language": {
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Proveedores",
+                "infoEmpty": "Mostrando 0 a 0 de 0 Proveedores",
+                "infoFiltered": "(Filtrado de _MAX_ total Proveedores)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Proveedores",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscador:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+            "responsive": true, "lengthChange": true, "autoWidth": false,
+            buttons: [
+                {
+                    extend: 'colvis',
+                    text: 'Visor de columnas',
+                    collectionLayout: 'fixed three-column'
+                }
+            ],
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
